7fb1dd8417a59c5e8bb5c17d9b88f422d026d3e9
Anwar Mujeeb <ibnmujeeb2003@gmail.com>
2024-04-28 11:41:37 -0700

    added simple profile page

--- App/src/App.js
+++ App/src/App.js
@@ -7,6 +7,7 @@ import ResourceMap from './pages/resourcemap';
 import SidebarLayout from './pages/sidebarlayout'; // Adjust path if necessary
 import Requests from './pages/requests.js'; // Import the Requests component
 import OffersPage from './pages/offerpage'; // Import the OffersPage component
+import ProfilePage from './pages/profilepage'; // Import the OffersPage component
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/map" element={<SidebarLayout><ResourceMap /></SidebarLayout>} />
         <Route path="/requests" element={<SidebarLayout><Requests/></SidebarLayout>} /> 
         <Route path="/offers" element={<SidebarLayout><OffersPage /></SidebarLayout>} /> 
+        <Route path="/profile" element={<SidebarLayout><ProfilePage /></SidebarLayout>} /> 
         {/* Additional routes can be added here */}
       </Routes>
     </Router>
